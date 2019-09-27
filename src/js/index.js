c21c3dde81ecae7863e39464e3a610d69d59a21e
luojianrong <luojianrong00@gmail.com>
2019-09-27 19:07:20 +0800

    完成了静态页面

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 //轮播
 let mySwiper = new Swiper('.swiper-container', {
   autoplay:true,//等同于以下设置
-  autoplay: {
+ /* autoplay: {
     delay: 2000,
     stopOnLastSlide: false,
     reverseDirection: false,
-    },
+    },*/
   pagination: {
     el: '.swiper-pagination',
   },
